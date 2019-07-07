c20840ef187b78218a402e36724fccda3c9b2163
John Lawal <jilawal@mum.edu>
2019-07-06 21:50:26 -0500

    reduced size

--- hw9/fifteen.js
+++ hw9/fifteen.js
@@ -1,6 +1,5 @@
 "use strict";
-(function() {
-    //Trick: X == Column == Left/100, Y == Row == Top/100
+(function() { //Trick: X == Column == Left/100, Y == Row == Top/100
     const NUM_OF_ROWS_COLS = 4; //number of rows and columns
     const PIECE_SIZE = 100; //width and height of each div
     var emptySquareRow = 3; //default row of empty div (0 based)
@@ -16,49 +15,36 @@
             let y = (Math.floor(index / NUM_OF_ROWS_COLS) * PIECE_SIZE);
 
             // set basic style and background
-            piece.addClass("puzzlepiece")
-                .css("left", x + "px")
-                .css("top", y + "px")
-                .css("background-image", "url('background.jpg')")
-                .css("background-position", -x + "px " + (-y) + "px");
+            piece.addClass("puzzlepiece").css("left", x + "px").css("top", y + "px")
+                .css("background-image", "url('background.jpg')").css("background-position", -x + "px " + (-y) + "px");
 
             // store x and y for later
             piece.row = y / PIECE_SIZE;
             piece.col = x / PIECE_SIZE;
-            piece.attr("id", "square_" + piece.row + "_" + piece.col)
-                .attr("row", piece.row)
-                .attr("col", piece.col);
+            piece.attr("id", "square_" + piece.row + "_" + piece.col).attr("row", piece.row).attr("col", piece.col);
         });
 
         //swap with empty if possible
         pieces.click(function() {
             var piece = $(this);
-            if (isMoveavablePiece(piece)) {
-                swapWithEmpty(piece);
-            }
+            if (isMoveavablePiece(piece)) swapWithEmpty(piece);
         });
 
         //hover effect for moveable pieces
         pieces.mouseover(function() {
             var piece = $(this);
-            if (isMoveavablePiece(piece)) {
-                piece.addClass("movablepiece");
-            } else {
-                piece.removeClass("movablepiece");
-            }
+            if (isMoveavablePiece(piece)) piece.addClass("movablepiece");
+            else piece.removeClass("movablepiece");
         });
 
-        //shuffle pieces
+        //shuffle pieces randomly
         $("#shufflebutton").click(function() {
-            //swap randomly
             var numOfTimes = Math.pow(NUM_OF_ROWS_COLS, 3);
             for (let a = 0; a < numOfTimes; a++) {
                 var moveablePieces = [];
                 for (let i = 0; i < NUM_OF_ROWS_COLS; i++) {
                     for (let j = 0; j < NUM_OF_ROWS_COLS; j++) {
-                        if (isMoveavableIndex(i, j)) {
-                            moveablePieces.push({ row: i, col: j });
-                        }
+                        if (isMoveavableIndex(i, j)) moveablePieces.push({ row: i, col: j });
                     }
                 }
                 var randomIndex = Math.floor(Math.random() * moveablePieces.length);
@@ -82,22 +68,17 @@
 
     //perform swap
     function swapWithEmpty(piece) {
-        if (isMoveavablePiece(piece)) { //just to be sure
-            var currentPieceRow = parseInt(piece.attr("row"));
-            var currentPieceCol = parseInt(piece.attr("col"));
+        var currentPieceRow = parseInt(piece.attr("row"));
+        var currentPieceCol = parseInt(piece.attr("col"));
 
-            piece.attr("row", emptySquareRow)
-                .attr("col", emptySquareCol)
-                .css("left", (emptySquareCol * PIECE_SIZE) + "px")
-                .css("top", (emptySquareRow * PIECE_SIZE) + "px")
-                .attr("id", "square_" + emptySquareRow + "_" + emptySquareCol);
+        piece.attr("row", emptySquareRow).attr("col", emptySquareCol).css("left", (emptySquareCol * PIECE_SIZE) + "px")
+            .css("top", (emptySquareRow * PIECE_SIZE) + "px").attr("id", "square_" + emptySquareRow + "_" + emptySquareCol);
 
-            emptySquareRow = currentPieceRow;
-            emptySquareCol = currentPieceCol;
-            checkGameStatus();
-        }
+        emptySquareRow = currentPieceRow;
+        emptySquareCol = currentPieceCol;
+        checkGameStatus();
     }
-
+    //check if user has won
     function checkGameStatus() {
         let pieces = $("#puzzlearea div");
         var expectedOutCome = [];
@@ -117,9 +98,7 @@
 
         var hasWon = (expectedOutCome.length === currentResult.length) && expectedOutCome.every((element, index) =>
             ((element.row === currentResult[index].row) && (element.col === currentResult[index].col)));
-        if (hasWon)
-            $("body").addClass("won");
-        else
-            $("body").removeClass("won");
+        if (hasWon) $("body").addClass("won");
+        else $("body").removeClass("won");
     }
 })();
